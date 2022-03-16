bbcfd77e05c363c50dafe3434e48cc01d7933435
yunyeongkim <yunyeong93@gmail.com>
2022-03-16 18:34:30 +0900

    fix 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          HELLO WORLD DOCKER COMPOSE
+          learn react
         </a>
       </header>
     </div>
